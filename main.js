e4e0e987a5ea6c5395fe0669d193c4064970614d
Michael-J-Fernandez <michael.fernandez@codeimmersives.com>
2022-09-30 18:52:36 -0400

    Complete main challenges. Stretch goals to be completed on next commit.

---
+++ main.js
@@ -0,0 +1,196 @@
+// ----- xify -----
+// returns the same string, but with every character replaced by an 'x'
+console.log("----- xify -----");
+
+
+function xify(str) {
+
+    let newStr = "";
+
+    for (i = 0; i < str.length; i++){
+    
+        newStr += "x";
+    }
+    return newStr;
+}
+
+console.log(xify("123"));
+
+
+
+
+// ----- yellingChars -----
+// returns the given string with an exclamation point after each character
+console.log("\n");
+console.log("----- yellingChars -----");
+
+
+function yellingChars(str) {
+
+    let newStr = "";
+
+    for (i = 0; i < str.length; i++){
+
+        newStr += str[i] + "!";
+    }
+    return newStr;
+}
+
+console.log(yellingChars("1234567"));
+
+
+
+
+// ----- indexChars -----
+// adds the index of each character before that character in the given string
+console.log("\n");
+console.log("----- indexChars -----");
+
+
+function indexChars(str) {
+
+    let newStr = "";
+
+    for (i = 0; i < str.length; i++){
+
+        newStr += i + str[i];
+    }
+    return newStr;
+}
+
+console.log(indexChars("abcdefg"));
+
+
+
+
+// ----- exclaim -----
+// returns the given sentence with every question mark or period changed to an exclamation point
+console.log("\n");
+console.log("----- exclaim -----");
+
+
+function exclaim(str) {
+
+    let newStr = "";
+    
+    for (i = 0; i < str.length; i++){
+
+        if (str[i] === "." || str[i] === "?") {
+            newStr += "!";
+
+        } else {
+
+            newStr += str[i];
+        }
+    }
+    return newStr;
+}
+
+console.log(exclaim("One. Two? Three. Four?"));
+
+
+
+// ----- truncate -----
+// shortens a long string to 15 characters plus an ellipsis (...)
+
+// PERSONAL NOTE: instructions for this assignment have been fulfilled, however the way my code is written on here, if str.length < 15, missing characters up until 15 will log "undefined"
+console.log("\n");
+console.log("----- truncate -----");
+
+
+function truncate(str) {
+
+    let newStr = "";
+
+    for (i = 0; i <= 15; i++){
+
+        newStr += str[i];
+
+    }
+    newStr += "(...)";
+    return newStr;
+}
+
+console.log(truncate("11111111111111111111111"));
+
+
+
+// ----- ciEmailify -----
+// returns the given sentence with every question mark or period changed to an exclamation point
+console.log("\n");
+console.log("----- ciEmailify -----");
+
+
+function ciEmailify(name) {
+
+    name = name.toLowerCase();
+    let eMail = "";
+
+    for (i = 0; i < name.length; i++){
+
+        if (name[i] === " ") {
+
+            eMail += ".";
+
+        } else {
+
+            eMail += name[i];
+        }
+    }
+    eMail += "@codeimmersives.com"
+    return eMail;
+}
+
+console.log(ciEmailify("Michael J Fernandez de la Cruz"));
+
+
+
+
+// ----- reverse -----
+// reverses the given string
+console.log("\n");
+console.log("----- reverse -----");
+
+function reverse(str) {
+    newStr = " ";
+    for (i = str.length - 1; i >= 0; i--){
+        newStr += str[i];
+        // console.log(newStr);
+    }
+    return newStr
+}
+
+console.log(reverse("0123456789"));
+
+
+
+// ----- onlyVowels -----
+// returns only the vowels from a word
+console.log("\n");
+console.log("----- onlyVowels -----");
+
+
+function onlyVowels(str) {
+
+    let newStr = "";
+
+    for (i = 2; i < str.length; i++){
+
+        if (str[i] === "A" ||
+            str[i] === "a" ||
+            str[i] === "E" ||
+            str[i] === "e" ||
+            str[i] === "I" ||
+            str[i] === "i" ||
+            str[i] === "O" ||
+            str[i] === "o" ||
+            str[i] === "U" ||
+            str[i] === "u") {
+            
+            newStr += str[i];
+        }
+    }
+    return newStr;
+}
+
+console.log(onlyVowels("GwAaGwEeGwIiGwOoGwUu"))
